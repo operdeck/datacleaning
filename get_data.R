2e534e0632df1240cc99df9ee005e3aab8c32069
Otto Perdeck <perdo@pega.com>
2015-01-22 17:19:34 +0100

    Downloading made OS agnostic

--- get_data.R
+++ get_data.R
@@ -1,3 +1,15 @@
 # Download the source data into the working folder
 fileURL <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
-download.file(fileURL, destfile = "./UCI HAR Dataset.zip", method="curl")
+
+# For Linux like systems, the 'curl' method needs to be specified
+# but for Windows, 'curl' cannot be specified - we're trying both
+
+tryCatch( {
+  download.file(fileURL, destfile = "./UCI HAR Dataset.zip", method="curl")
+}, finally = {
+})
+
+tryCatch( {
+  download.file(fileURL, destfile = "./UCI HAR Dataset.zip")
+}, finally = {
+})
